b58094baaf5eceb7621bae0a6697e65411785888
= <=>
2023-06-08 12:17:41 +0900

    rr

--- js/gallery.js
+++ js/gallery.js
@@ -3,7 +3,7 @@ const wrap = document.querySelector('.gallery .wrap');
 const api_key = 'b6a8ab876282930a394fe90be640af10';
 const password = '869efeb15881763d';
 const method_interest = 'flickr.interestingness.getList';
-const num = 5;
+const num = 15;
 const baseURL = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&format=json&nojsoncallback=1&per_page=${num}`;
 
 fetch(baseURL)
@@ -20,11 +20,18 @@ fetch(baseURL)
                     <a href='https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg'>
                         <img src='https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg' />
                     </a>
-                    <p>${item.title}</p>
+                    <p>${item.title === '' ? 'have a good day !' : item.title}</p>
                 </div>
             </li>
         `;
     })
 
     wrap.innerHTML = tags;
+
+    new Isotope( wrap, {
+        // options
+        itemSelector: '.item',
+        transitionDuration: '0.5s',
+        layoutMode: 'fitRows'
+      });
 })
