ec1e832006c8426733869ebbce289a1c40367fc4
= <=>
2023-06-09 10:43:46 +0900

    대체이미지

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,7 @@
 const wrap = document.querySelector('.gallery .wrap');
 const loading = document.querySelector('.gallery .loading');
 const api_key = 'ae5dbef0587895ed38171fcda4afb648';
-const num = 500;
+const num = 50;
 const myId = '198560836@N08';
 const baseURL = `https://www.flickr.com/services/rest/?format=json&nojsoncallback=1&api_key=${api_key}&per_page=${num}&method=`;
 const method_interest = 'flickr.interestingness.getList';
@@ -13,6 +13,7 @@ fetch(user_url)
 	.then((res) => res.json())
 	.then((json) => {
 		const items = json.photos.photo;
+
 		let tags = '';
 
 		items.forEach((item) => {
@@ -20,9 +21,14 @@ fetch(user_url)
         <li class='item'>
           <div>
             <a href='https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg'>
-              <img src='https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg' />
+              <img class="thumb" src='https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg' />
             </a>
             <p>${item.title === '' ? 'Have a good day!!' : item.title}</p>
+
+			<article class="profile">
+				<img src="http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg" />
+				<span>${item.owner}</span>
+			</article>
           </div>
         </li>
       `;
@@ -34,10 +40,14 @@ fetch(user_url)
 		let count = 0;
 
 		for (const el of imgs) {
+			el.onerror = () => {
+				el.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif');
+			};
 			el.onload = () => {
 				count++;
 				count === imgs.length && isoLayout();
 			};
+
 		}
 	});
 
