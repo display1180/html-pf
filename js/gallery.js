9d110c6826a718dce897337da3549c46bfa4ed73
= <=>
2023-06-09 11:17:58 +0900

    searchbox styling

--- js/gallery.js
+++ js/gallery.js
@@ -1,58 +1,65 @@
 const wrap = document.querySelector('.gallery .wrap');
 const loading = document.querySelector('.gallery .loading');
 const api_key = 'ae5dbef0587895ed38171fcda4afb648';
-const num = 50;
-const myId = '198560836@N08';
+const num = 500;
+const myId = '164021883@N04';
 const baseURL = `https://www.flickr.com/services/rest/?format=json&nojsoncallback=1&api_key=${api_key}&per_page=${num}&method=`;
 const method_interest = 'flickr.interestingness.getList';
 const method_user = 'flickr.people.getPhotos';
 const interest_url = `${baseURL}${method_interest}`;
 const user_url = `${baseURL}${method_user}&user_id=${myId}`;
 
-fetch(user_url)
-	.then((res) => res.json())
-	.then((json) => {
-		const items = json.photos.photo;
+fecthData(interest_url);
 
-		let tags = '';
+async function fecthData(url) {
+	const res = await fetch(url);
+	const json = await res.json();
+	const items = json.photos.photo;
+	console.log(items);
 
-		items.forEach((item) => {
-			tags += `
+	createList(items);
+}
+
+function createList(arr) {
+	let tags = '';
+
+	arr.forEach((item) => {
+		tags += `
         <li class='item'>
           <div>
             <a href='https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg'>
-              <img class="thumb" src='https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg' />
+              <img class='thumb' src='https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg' />
             </a>
             <p>${item.title === '' ? 'Have a good day!!' : item.title}</p>
-
-			<article class="profile">
-				<img src="http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg" />
-				<span>${item.owner}</span>
-			</article>
+				<article class='profile'>	
+					<img src='http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg' />				
+					<span>${item.owner}</span>
+				</article>
           </div>
         </li>
       `;
-		});
-
-		wrap.innerHTML = tags;
-
-		const imgs = wrap.querySelectorAll('img');
-		let count = 0;
-
-		for (const el of imgs) {
-			//만약 이미지에 엑박이 뜨면, onerror 이벤트로 잡아서, 디폴트 이미지로 대체
-			el.onerror = () => {
-				el.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif');
-			};
-
-			//디폴트로 변경된 이미지까지 포함해서 카운트(무한로딩에 빠지지 않음)
-			el.onload = () => {
-				count++;
-				count === imgs.length && isoLayout();
-			};
-
-		}
 	});
+	wrap.innerHTML = tags;
+
+	setLoading();
+}
+
+function setLoading() {
+	const imgs = wrap.querySelectorAll('img');
+	let count = 0;
+
+	for (const el of imgs) {
+		//만약 이미지에 엑박이 뜨면 onerror이벤트로 잡아서 디폴트 이미지로 대체
+		el.onerror = () => {
+			el.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif');
+		};
+		//디폴트로 변경된 이미지까지 포함해서 카운트 (무한로딩에 빠지지 않음)
+		el.onload = () => {
+			count++;
+			count === imgs.length && isoLayout();
+		};
+	}
+}
 
 function isoLayout() {
 	new Isotope(wrap, {
